1997285f79a2c593e57a5893bfce938474a16a60
Taha Loghmari <medtahalog@gmail.com>
2024-12-25 12:17:23 +0100

    Finished Exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function( a , b ) {
+    if ( a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) ) return "ERROR" ;
+    if ( a > b ) [a,b] = [b,a] ;
+    return b*(b+1)/2  - a*(a-1)/2 ; 
 };
 
 // Do not edit below this line
