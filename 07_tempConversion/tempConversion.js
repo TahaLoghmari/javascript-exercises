b23f6c9530936c0c896470c74eec8dbb8ecc9a50
Taha Loghmari <medtahalog@gmail.com>
2024-12-25 12:27:50 +0100

    Finished Exercice 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function( F ) {
+  let number = ( F - 32 ) * (5/9) ; 
+  return Math.round(number * 10) / 10 ; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( C ) {
+  let number = ( C * (9/5) + 32 ) ; 
+  return Math.round(number * 10) / 10 ; 
 };
 
 // Do not edit below this line
