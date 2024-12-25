a40622be814bff9f6f6f91e68fc5b498dedc20a0
Taha Loghmari <medtahalog@gmail.com>
2024-12-25 11:33:15 +0100

    finished 02_repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function( message , num ) {
+    if ( num < 0 ) return "ERROR" ; 
+    let Result = "";
+    for ( let i = 0 ; i < num ; ++ i ) Result += message;
+    return Result ; 
 };
 
 // Do not edit below this line
